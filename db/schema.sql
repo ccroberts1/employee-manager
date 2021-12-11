4685629b9ad2313e0a8c368d408591fcc250ba2a
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-12-11 12:06:33 -0700

    added foreign keys to tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,30 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) --holds department name
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL --holds department name
 )
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30), --holds role title
     salary DECIMAL, --holds role salary
-    department_id INT --holds reference to department role belongs to
+    department_id INT, --holds reference to department role belongs to
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 )
 
 CREATE TABLE employee (
     id INT PRIMARY KEY,
-    first_name VARCHAR(30), --holds employee first name
-    last_name VARCHAR(30), --holds employee last name
-    role_id INT, --holds reference to employee role
-    manager_id INT --holds reference to another employee that's a manager of the current employee, should be ull if the employee has no manager
+    first_name VARCHAR(30), --holds emp first name
+    last_name VARCHAR(30), --holds emp last name
+    role_id INT, --holds reference to emp role
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+    manager_id INT, --holds ref to another employee who manages current emp
+    FOREIGN KEY (id) 
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 )
